6f9f99b93e133ac99963359518bd3c695afd1d29
WALMIR FERNANDES <39130044+walmir35@users.noreply.github.com>
2019-08-01 20:44:48 -0300

    Add files via upload

---
+++ server/app.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const path = require('path')
+const cors = require('cors');
+
+const bodyParser = require('body-parser')
+app.use(bodyParser.json({ extended: true }))
+
+const routes = require('./routes')
+app.use(cors());
+app.use(routes)
+
+if (process.env.NODE_ENV === 'production') {
+  const baseDir = process.env.BASE_DIR || './' // /usr/src/app/
+  // Serve any static files
+  app.use(express.static(baseDir + 'build'))
+  // Handle React routing, return all requests to React app
+  app.get('*', function (req, res) {
+    res.sendFile(baseDir + 'build/index.html')
+  })
+}
+
+module.exports = app
